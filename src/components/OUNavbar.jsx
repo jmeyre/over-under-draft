1cb098d47e4fe39abad1df20e76ebb617d518c72
John Eyre <jmeyre1998@gmail.com>
2022-01-06 16:19:14 -0600

    fixed navbar logo

--- src/components/OUNavbar.jsx
+++ src/components/OUNavbar.jsx
@@ -1,10 +1,11 @@
 import { Container, Navbar } from "react-bootstrap";
+import logo from '../nba.png';
 
 const OUNavbar = () => (
   <Navbar bg="dark" variant="dark">
     <Container>
       <Navbar.Brand>
-        <img alt="NBA Logo" src="%PUBLIC_URL%/nba.png" width="50" height="50" />
+        <img alt="NBA Logo" src={logo} width="50" height="50" />
         Buckets 2021-2022 Over/Under Draft Progress Board
       </Navbar.Brand>
     </Container>

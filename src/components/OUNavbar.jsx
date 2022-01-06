cf2e9fe0c39f667449b22567a116f13060d8c864
John Eyre <jmeyre1998@gmail.com>
2022-01-06 16:11:40 -0600

    title and logo metadata

--- src/components/OUNavbar.jsx
+++ src/components/OUNavbar.jsx
@@ -4,7 +4,7 @@ const OUNavbar = () => (
   <Navbar bg="dark" variant="dark">
     <Container>
       <Navbar.Brand>
-        <img alt="NBA Logo" src="http://a4.espncdn.com/combiner/i?img=%2Fi%2Fespn%2Fmisc_logos%2F500%2Fnba.png" width="50" height="50" />
+        <img alt="NBA Logo" src="%PUBLIC_URL%/nba.png" width="50" height="50" />
         Buckets 2021-2022 Over/Under Draft Progress Board
       </Navbar.Brand>
     </Container>

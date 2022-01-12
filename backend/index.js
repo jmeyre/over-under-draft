7b7f6a5e957670623d2b5635bd973c5151d612a5
John Eyre <jmeyre1998@gmail.com>
2022-01-11 19:48:24 -0600

    edited backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,10 @@ const path = require("path");
 const express = require("express");
 const { apiCaller, getData } = require( "./functions/api.js");
 
-const PORT = process.env.PORT || 80;
-
 const app = express();
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 apiCaller();
 
@@ -17,11 +15,13 @@ app.get("/api", (req, res) => {
   res.send({ data });
 });
 
-// All other GET requests not handled before will return our React app
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);

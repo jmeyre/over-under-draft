1a8319f8ceb55bb7f423fd90547590c87b396821
John Eyre <jmeyre1998@gmail.com>
2025-11-06 22:10:43 -0500

    update deps and add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import MainPage from './components/MainPage';
 import OUNavbar from './components/OUNavbar';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => (
   <div className="App">
     <OUNavbar />
     <div style={{ marginTop: '12px' }}>
       <MainPage />
+      <Analytics />
     </div>
   </div>
 )
